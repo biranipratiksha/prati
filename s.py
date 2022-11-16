1f7219594d41384a4b3e9ca12750835868c2b0ea
biranipratiksha <117799737+biranipratiksha@users.noreply.github.com>
2022-11-16 04:07:03 -0500

    Create s.py

---
+++ s.py
@@ -0,0 +1,14 @@
+# Python3 code to demonstrate
+# to count words in string
+ 
+# initializing string
+test_string = "Geeksforgeeks is best Computer Science Portal"
+ 
+# printing original string
+print ("The original string is : " + test_string)
+ 
+# to count words in string
+res = test_string.count(" ")+1
+ 
+# printing result
+print ("The number of words in string are : " + str(res))
